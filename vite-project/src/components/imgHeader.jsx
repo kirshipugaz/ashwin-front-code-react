28632e30b83e0539236e875036c6855d421ef010
Ashwin <ashwin.madavan123@gmail.com>
2024-06-05 12:28:50 +0530

    login/signup page footer and image fixes

--- vite-project/src/components/imgHeader.jsx
+++ vite-project/src/components/imgHeader.jsx
@@ -5,7 +5,7 @@ import unite from "../assets/unite.png"
 function Header() {
   return (
     <header className="header-content">
-      <img src={unite} alt="Join the circle of wellness" className="img-fluid header-image" />
+      <img src={unite} alt="Join the circle of wellness" className="header-image" />
     </header>
   );
 }
